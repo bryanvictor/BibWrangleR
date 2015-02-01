8a2873b7869c67a10fd53501b9238debd4fba9d7
Brian Perron <beperron@umich.edu>
2015-01-31 23:17:50 -0500

    Heavy cleaning. All is functional.

--- Functions/ebscoBWR.R
+++ Functions/ebscoBWR.R
@@ -17,9 +17,13 @@ ebscoBWR.f <- function(csv = FALSE, path){
 #     at the end each text file.  Right now, a full carriage return must be
 #     included, and no further quality checks are made.
 #
-#  6. Include in the ebscoBWR function call an argument that would allow for
-#     an additional reduced subset of data to be processed for manual
-#     inspection.
+#  6. Double check logic of section 4. Can we confirm that JN and SO are
+#     problematic?
+#
+#  7. In Section 5, perform a test to ensure all APA codes are digits before
+#     removing.
+#
+#  8.
 #_______________________________________________________________________________
 
 
@@ -159,88 +163,107 @@ section.3 <- end.time-start.time
 #-------------------------------------------------------------------------------
 #
 #Journals that have a Special Issue are not grouped together as the same
-#title. These subtitles need to be removed.
+#title. These subtitles need to be removed.  Some articles have both the JN
+#and SO code.  Thus, keeping both results in an excess count. A quality control
+#check is to ensure the number of article titles is exactly equal to the
+#unique number of journal title entries
 #_______________________________________________________________________________
 
 start.time <- Sys.time()
 
-DF$record[DF$attributes == "SO"] <- gsub(" Special Issue", "", DF$record[DF$attributes == "SO"])
-DF$record[DF$attributes == "SO"] <- gsub(":.*", "", DF$record[DF$attributes == "SO"])
+    DF$record[DF$attributes == "SO"] <- gsub(" Special Issue", "",
+        DF$record[DF$attributes == "SO"])
 
-DF$record[DF$attributes == "JN"] <- gsub(" Special Issue", "", DF$record[DF$attributes == "JN"])
-DF$record[DF$attributes == "JN"] <- gsub(":.*", "", DF$record[DF$attributes == "JN"])
+    DF$record[DF$attributes == "SO"] <- gsub(":.*", "",
+        DF$record[DF$attributes == "SO"])
 
+    DF$record[DF$attributes == "JN"] <- gsub(" Special Issue", "",
+        DF$record[DF$attributes == "JN"])
 
-#DF$record <- if(DF$record == "JN"){"SO")
+    DF$record[DF$attributes == "JN"] <- gsub(":.*", "",
+        DF$record[DF$attributes == "JN"])
 
+    #Create separate data files filtered by SO and JN, and a set of unique ID's
+    journal.unique.SO <- filter(DF, attributes == "SO")
+    journal.unique.JN <- filter(DF, attributes == "JN")
+    articleID.unique <- unique(DF$articleID)
 
+    #Which ID's overlap from JN to SO? This shows articles with both JN and SO
+    #fields.
+    JN.in.SO <- journal.unique.JN$articleID %in% journal.unique.SO$articleID
 
+    #Filter out the ID from the JN that overlap with SO
+    journal.unique.JN <- journal.unique.JN[!(JN.in.SO), ]
+    journal.unique.JN <- mutate(journal.unique.JN, attributes = "SO")
+    DF <- filter(DF, attributes != "JN")
+    DF <- rbind(DF, journal.unique.JN)
 
-
-
-#Identify which journals have the JN code but not the SO code.
-articleID.unique <- unique(DF$articleID)
-journal.unique.SO <- filter(DF, attributes == "SO")
-journal.unique.JN <- filter(DF, attributes == "JN")
-
-#Which ID's overlap from JN to SO?
-JN.in.SO <- journal.unique.JN$articleID %in% journal.unique.SO$articleID
-
-#Filter out the ID from the JN that overlap with SO
-journal.unique.JN <- journal.unique.JN[!(JN.in.SO), ]
-journal.unique.JN <- mutate(journal.unique.JN, attributes = "SO")
-
-DF <- filter(DF, attributes != "JN")
-
-DF <- rbind(DF, journal.unique.JN)
-
+    rm(journal.unique.SO, journal.unique.JN, articleID.unique, JN.in.SO)
 
 end.time <- Sys.time()
 section.4 <- end.time-start.time
-#____________________________5. COMBINE Year FIELDS________________________________
+
+
+#_______________________________________________________________________________
+#                        5. COMBINE YEAR FIELDS
+#-------------------------------------------------------------------------------
+#
+#
+#_______________________________________________________________________________
 
 start.time <- Sys.time()
 
-DF$attributes <- ifelse(DF$attributes == "PY", "YR", DF$attributes)
+    DF$attributes <- ifelse(DF$attributes == "PY", "YR", DF$attributes)
 
-DF.temp <- DF
+    DF.temp <- DF
 
-DF.temp <- filter(DF.temp, attributes == "PD")
+    DF.temp <- filter(DF.temp, attributes == "PD")
 
 
-# APA appears to use a 6 to 8 digit identifier that needs to be excluded
-DF.temp$record <- ifelse(nchar(DF.temp$record) >= 6 & DF.temp$attributes == "PD" &
-                         nchar(DF.temp$record) <= 8 & DF.temp$attributes == "PD",
-                         "", DF.temp$record)
+    # APA appears to use a 6 to 8 digit identifier that needs to be excluded
+    DF.temp$record <- ifelse(nchar(DF.temp$record) >= 6 &
+                             DF.temp$attributes == "PD" &
+                             nchar(DF.temp$record) <= 8 &
+                             DF.temp$attributes == "PD",
+                             "",
+                             DF.temp$record)
 
-# Extract the first portion of the dates, up to the point with a 2 or 4 digit
-# year value. This also captures some letters and characters.
-DF.temp$record <- stringr::str_extract(DF.temp$record, "[$/A-Za-z0-9]+\\d{2,4}")
+    # Extract the first portion of the dates, up to the point with a 2 or 4
+    # digit year value. This also captures some letters and characters.
+    DF.temp$record <- stringr::str_extract(DF.temp$record,
+                       "[$/A-Za-z0-9]+\\d{2,4}")
 
-#Exclude the characters
-DF.temp$record <- gsub("[/A-Za-z]", "", DF.temp$record)
+    #Exclude the characters
+    DF.temp$record <- gsub("[/A-Za-z]", "", DF.temp$record)
 
-#Add 19 to all the records with just two digits
-
-DF.temp <- DF.temp[!is.na(DF.temp$record), ]
-DF.flag <- mutate(DF.temp, flag = ifelse(nchar(DF.temp$record) == 2, "1", "0" )) %>%
+    #Add 19 to all the records with just two digits
+    DF.temp <- DF.temp[!is.na(DF.temp$record), ]
+    DF.flag <- mutate(DF.temp, flag = ifelse(nchar(DF.temp$record) == 2, "1", "0" )) %>%
            filter(flag == "1") %>%
            group_by(record) %>%
            summarize(N = n())
 
-colnames(DF.flag)<-c("Year", "N")
+    colnames(DF.flag)<-c("Year", "N")
 
 
-DF.temp$record <- ifelse(as.numeric(DF.temp$record) < 100, paste("19", DF.temp$record, sep=""), DF.temp$record)
-DF.temp <- mutate(DF.temp, attributes = "YR")
+    DF.temp$record <- ifelse(as.numeric(DF.temp$record) < 100,
+                      paste("19", DF.temp$record, sep=""), DF.temp$record)
+    DF.temp <- mutate(DF.temp, attributes = "YR")
 
-DF <- rbind(DF, DF.temp)
-DF <- arrange(DF, articleID)
+    DF <- rbind(DF, DF.temp)
+    DF <- arrange(DF, articleID)
+
+    rm(DF.temp)
 
 end.time <- Sys.time()
 section.5 <- end.time-start.time
-#____________________________________________________________________________________________
 
+#_______________________________________________________________________________
+#                        5. OUTPUT
+#-------------------------------------------------------------------------------
+#
+#
+#_______________________________________________________________________________
 
 bwr.df <<- DF
 if(csv == TRUE){write.csv(pi.df, "pi.csv")}
