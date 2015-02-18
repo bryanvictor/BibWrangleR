5922951206867c0b1a27db078bac5c7a8cf40b43
bryanvictor <bryanvictor@gmail.com>
2015-02-17 21:08:15 -0500

    Author field clean up
    Adds code to strip out author biographies from record field

--- Functions/proquestBWR.R
+++ Functions/proquestBWR.R
@@ -77,6 +77,11 @@ proQuestBWR.f <- function(csv = FALSE, path){
         DF.authors.fixed <- split.df
         DF.no.authors <- filter(full.df, attributes != "Author")
 
+        #Strip biographies from author fields in DF.author.good data frame
+        library(qdap)
+        DF.authors.good[,"record"]<-beg2char(DF.authors.good$record, "1", 1)
+        detach(qdap)
+    
         #Bind the reduced DF with the fixed df
         DF <- rbind(DF.no.authors, DF.authors.good, DF.authors.fixed)
         DF <- arrange(DF, articleID)
